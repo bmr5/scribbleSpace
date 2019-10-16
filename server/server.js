243be4ca034e30fe59a07fd636179ac428d933cc
prasanna malla <mallaprasanna@gmail.com>
2019-10-16 11:57:56 -0400

    fix conflicts via merge conflicts

--- server/server.js
+++ server/server.js
@@ -2,11 +2,10 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+require('dotenv').config();
 const port = process.env.PORT || 3000;
 const path = require('path');
 const cookieParser = require('cookie-parser');
-require('dotenv').config();
-
 
 // const roomCtrl = require('./controllers/roomCtrl');
 const userCtrl = require('./controllers/userCtrl');
@@ -29,12 +28,17 @@ app.use(express.static(path.join(__dirname, 'src')));
 //   res.sendFile(path.resolve(__dirname, '../src/index.html'));
 // });
 
-app.post('/login', userCtrl.test, (req, res, next) => {
+app.post('/login', userCtrl.checkLogin, (req, res, next) => {
   console.log('login on express', req.body);
 });
 
 app.use('/', (req, res) => {
   res.status(200).sendFile(path.resolve(__dirname, './../src/index.html'));
+});
+
+app.post('/test', userCtrl.updatePassword, (req, res, next) => {
+  console.log(res.locals.updateComplete);
+  res.status(200).send(JSON.stringify(res.locals));
 });
 
 // standard bad endpoint, send 404
@@ -48,11 +52,7 @@ app.use((err, req, res, next) => {
     status: 500,
     message: 'Default Error from the Global Error Handler'
   };
-
-app.post('/test', userCtrl.updatePassword, (req, res,next) => {
-  console.log(res.locals.updateComplete)
-  res.status(200).send(JSON.stringify(res.locals))
-})
+});
 
 function onConnection(socket) {
   socket.on('transfer', data => io.emit('broadcast', data));
