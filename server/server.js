2ba967e79e2b470d7a4eb5e8757ae6e81db85591
prasanna malla <mallaprasanna@gmail.com>
2019-10-16 11:32:04 -0400

    fix web-dev-server

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 const path = require('path');
 const cookieParser = require('cookie-parser');
+require('dotenv').config();
+
 
 // const roomCtrl = require('./controllers/roomCtrl');
 const userCtrl = require('./controllers/userCtrl');
@@ -12,24 +14,47 @@ const userCtrl = require('./controllers/userCtrl');
 app.use(express.json());
 app.use(cookieParser());
 
-
 //serve the bundle
-app.use(express.static(path.resolve(__dirname, '../dist')), (req, res, next)=>{
-  next()
-}); //
+app.use(express.static(path.join(__dirname, 'src')));
 
-app.get('/dist/bundle.js', (req,res,next)=>{
-  res.sendFile(path.resolve(__dirname, '../dist/bundle.js'))
-})
+// app.use(express.static(path.resolve(__dirname, '../dist')), (req, res, next)=>{
+//   next()
+// }); //
 
-app.get('/', (req, res, next) => {
-  res.sendFile(path.resolve(__dirname, '../src/index.html'))
-})
+// app.get('/dist/bundle.js', (req, res, next) => {
+//   res.sendFile(path.resolve(__dirname, '../dist/bundle.js'));
+// });
 
-app.post('/login', userCtrl.test, (req,res,next)=>{
-  console.log('login on express', req.body)
-})
+// app.get('/', (req, res, next) => {
+//   res.sendFile(path.resolve(__dirname, '../src/index.html'));
+// });
 
+app.post('/login', userCtrl.test, (req, res, next) => {
+  console.log('login on express', req.body);
+});
+
+app.use('/', (req, res) => {
+  res.status(200).sendFile(path.resolve(__dirname, './../src/index.html'));
+});
+
+// standard bad endpoint, send 404
+app.use('*', (req, res) => {
+  res.status(404).send('no route found');
+});
+
+// global error handler
+app.use((err, req, res, next) => {
+  const defaultError = {
+    status: 500,
+    message: 'Default Error from the Global Error Handler'
+  };
+
+  console.log('global error handler triggered');
+  const assignError = { ...defaultError, ...err };
+
+  // send the response
+  res.status(assignError.status).send(assignError.message);
+});
 
 function onConnection(socket) {
   socket.on('transfer', data => io.emit('broadcast', data));
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -2,42 +2,40 @@ import React from 'react';
 // Creates LoginForm, allows for input of username,password, and room number.
 function LoginForm(props) {
   return (
-    <div className="loginBox">
-
+    <div className='loginBox'>
       <form onSubmit={props.handleSubmit}>
-        <div className="inputBoxOnLoginComponent">
+        <div className='inputBoxOnLoginComponent'>
           <label>
             {/* Username input field */}
             <input
-              placeholder="Username"
-              type="text"
+              placeholder='Username'
+              type='text'
               value={props.name}
               onChange={props.handleChangeName}
             />
           </label>
-          <label>
-            {/* Room name input field */}
+          {/* <label>
             <input
               placeholder="Room Name"
               type="text"
               value={props.roomName}
               onChange={props.handleChangeRoom}
             />
-          </label>
+          </label> */}
           <label>
             {/* Password input field */}
             <input
-              placeholder="Password"
-              type="text"
+              placeholder='Password'
+              type='text'
               value={props.password}
               onChange={props.handleChangePassword}
             />
           </label>
           {/* Main submit button */}
-          <input id="loginButton" type="submit" value="Login" />
+          <input id='loginButton' type='submit' value='Login' />
         </div>
       </form>
-      <img id="scribbleLogo" src="scribble-svgrepo-com.svg" alt="" />
+      <img id='scribbleLogo' src='scribble-svgrepo-com.svg' alt='' />
     </div>
   );
 }
